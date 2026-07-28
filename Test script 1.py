fcf100b1423838728c656cd85689a99d3b6b4992
verycoolhacker <ewenavery@gmail.com>
2026-07-28 13:31:08 -0400

    Add files via upload

---
+++ Test script 1.py
@@ -0,0 +1,6 @@
+print("Welcome to *open* world game")
+input1 = input("do you wish to go left or right")
+if input1 == "left":
+    print("you started walking left onto the path before you got sucked into your phone and fell into a 9 meter deep drop severely injuring you")
+if input1 == "right":
+    print("you encountered milo and he kidnapped you killing you")
