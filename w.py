0fd681ea4f6cf482eb88d1c0d55269c8960d4239
verycoolhacker <ewenavery@gmail.com>
2026-07-28 13:28:26 -0400

    Add files via upload

---
+++ w.py
@@ -0,0 +1,73 @@
+#Script demo 1 !TO BE IMPROVED!
+import sys
+import time
+
+print("Welcome to the game of conciousness")
+name = input("""whats your name?
+""")
+print(name, "! interesting name..")
+time.sleep(2.5)
+print("""you wake up in a cold empty cellar
+      with no way to see whats the first thing
+      you do?""")
+question1 = input("""
+                  1, you scream
+                  2, you give up
+                  3, you look around trying to find something to help you see
+
+                  """)
+
+if question1 == "1":
+    print("""a ugly orc comes downstairs and stare at you..
+        you dont know what its going todo before it lunges
+        at you ripping your heart out and killing you""")
+    time.sleep(1)
+    sys.exit()
+if question1 == "2":
+    print("""as you look around you say to
+         yourself fuck me... before you start crying
+         accepting your fate and you spend the rest of your days starving""")
+if question1 == "3":
+    print("as you feel around your hand touches a hard metal object and you pick it up only to discover its a flashlight!")
+    time.sleep(1)
+print("as you get the flashlight to turn on you start hearing big.. heavy.. scary footsteps.. what do you do?")
+question2 = input   ("""
+                     1, you try to hide the flashlight and pretend your knocked out
+                     2, you get up and try to hide behind the door planning to ambush whatever that sound is coming from
+                     3, you scream to whatever is coming the following phrase "DONT COME IN IM NAKED"
+                     """)
+
+if question2 == "1":
+    print("""
+    a green stinky orc opens the door and says "i can smell someone whos faking to be asleep.."
+   the orc then walks up to you and rips your eyes out causing you to scream in agony begging for mercy
+  but it doesent stop it taunts you as you bleed out fading out of conciousness
+            """)
+if question2 == "2":
+    print("""as the thing comes downstairs and opens the door
+             you get the chance to come behind it and choke it for a couple seconds it trys to fight but you it even trys
+             to pull a knife but in the end you disarm it and feel it go limp so you let go
+             as you get a good look you see how its a orc and something feels wrong.. its dimensions..
+             they dont feel normal..they feel.. E>R?R??O?R 0.0.0.0.0.0.0.0.0""")
+    time.sleep(10)
+    print("you:w-what happened?")
+if question2 == "3":
+    print("""as you scream your naked the footsteps seem to stop and it becomes eerily quiet.. almost
+            to quiet... as you stare at the door waiting for something to happen you witness as the door
+            slowly creeks open revealing a horrifying orc standing with a butcher knife and as you try to
+            scream for mercy it lunges slitting your throat and forcing you to stare it in the eyes as you
+            feel your blood spurt out onto your rugged clothes making your lose conciousness and die""")
+
+time.sleep(5)
+print("THEM:sweetie its ok THEY:mam your son is still recovering from the ACC1DENT THEM:sweetie im so sorry this had to happen everything will be ok soon")
+time.sleep(1)
+print("i dont know who they are i feel that i need to defend myself from these monsters")
+question3 = input("""You feel your anger and fear course through your veins what do you do?
+
+                    1, KILL THEM ALL
+                    2, KILL THEM ALL
+                    3, KILL THEM ALL
+                    """)
+
+if question3 = "1":
+    print("you ravange through their souls trying to find the truth yet the truth was always there")
